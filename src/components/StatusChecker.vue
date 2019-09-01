29a5756321ed622ad6272813512d5d02e4307424
SotaSuzuki <3.3sota.suzuki@gmail.com>
2019-09-01 18:40:46 +0900

    add using ref example

--- src/components/StatusChecker.vue
+++ src/components/StatusChecker.vue
@@ -7,20 +7,21 @@
 </template>
 
 <script lang="ts">
-import Vue, { PropOptions } from 'vue'
+import Vue, { PropType } from 'vue'
 import { computed } from '@vue/composition-api'
 import { Status } from '~/hooks/useChangeStatus'
 
 export default Vue.extend({
   props: {
     status: {
-      type: String,
+      type: String as PropType<string>,
       required: false,
       default: Status.TODO,
-    } as PropOptions<string>,
+    },
   },
 
   setup(props) {
+    // const { status } = props // NOTE: 分割代入すると computed が動かない？
     const statusMsg = computed(() => `今の状態は ${props.status} どぇす。`)
     return {
       statusMsg,
--- src/pages/playground.vue
+++ src/pages/playground.vue
@@ -1,12 +1,13 @@
 <template>
   <div>
-    <h1>TypeScript Playground</h1>
+    <h1 ref="titleRef">TypeScript Playground</h1>
     <button type="button" @click="onClickChangeStatusBtn">Change Status</button>
     <StatusChecker :status="status" />
   </div>
 </template>
 
 <script lang="ts">
+import { ref, onMounted } from '@vue/composition-api'
 import useChangeStatus from '~/hooks/useChangeStatus'
 import StatusChecker from '~/components/StatusChecker.vue'
 
@@ -16,15 +17,22 @@ export default {
   },
 
   setup() {
+    const titleRef = ref(null)
     const { status, toggleStatus } = useChangeStatus()
 
     const onClickChangeStatusBtn = () => {
       toggleStatus()
     }
 
+    onMounted(() => {
+      const el: any = titleRef.value
+      el.style.color = '#338'
+    })
+
     return {
       status,
       onClickChangeStatusBtn,
+      titleRef,
     }
   },
 }
